bc6bc5709f6f9e493ff1c1d0337cb4f508b56152
Pramod JM <pramodjm4@gmail.com>
2024-10-10 07:30:47 +0530

    local storage issue

--- components/navbar/LaptopNav.tsx
+++ components/navbar/LaptopNav.tsx
@@ -14,42 +14,39 @@ export const LaptopNav = () => {
     <div className="min-h-14 border w-full flex items-center justify-between px-2  ">
       <h1>LOGO</h1>
       <ul className="min-h-14   flex items-center justify-between gap-5  ">
-        {localStorage.getItem("token") && (
-          <>
-            <li>
-              <Button
-                variant={"outline"}
-                onClick={() => router.push("/create-notes")}
-              >
-                Create Notes
-              </Button>
-            </li>
+        <>
+          <li>
+            <Button
+              variant={"outline"}
+              onClick={() => router.push("/create-notes")}
+            >
+              Create Notes
+            </Button>
+          </li>
 
-            <li>
-              <Button
-                variant={"outline"}
-                onClick={() => router.push("/fetch-notes")}
-              >
-                Fetch Notes
-              </Button>
-            </li>
-          </>
-        )}
+          <li>
+            <Button
+              variant={"outline"}
+              onClick={() => router.push("/fetch-notes")}
+            >
+              Fetch Notes
+            </Button>
+          </li>
+        </>
+
         <li>
           <Button variant={"outline"} onClick={() => router.push("/")}>
             Home{" "}
           </Button>{" "}
         </li>
         <li>
-          {localStorage.getItem("token") ? (
-            <>
-              <Button variant={"outline"} onClick={logoutfn}>
-                Logout
-              </Button>
-            </>
-          ) : (
-            <Button onClick={() => router.push("/login")}> Login</Button>
-          )}
+          <>
+            <Button variant={"outline"} onClick={logoutfn}>
+              Logout
+            </Button>
+          </>
+
+          <Button onClick={() => router.push("/login")}> Login</Button>
         </li>
       </ul>
     </div>

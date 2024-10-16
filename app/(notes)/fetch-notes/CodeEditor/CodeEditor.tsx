d73c233e9724bedc9a6a8d8768a6f0fa5ca1302c
Pramod JM <pramodjm4@gmail.com>
2024-10-16 18:48:43 +0530

    handle empty value in fetch page

--- app/(notes)/fetch-notes/CodeEditor/CodeEditor.tsx
+++ app/(notes)/fetch-notes/CodeEditor/CodeEditor.tsx
@@ -35,15 +35,15 @@ export default function FetchCodeEditor(props: PropsType) {
     programingLanguage,
     individualSubjectDataList,
   } = props;
+
   const mountFn = (editor: any) => {
     editorRef.current = editor;
-    // editor.focus();
   };
 
   const [languageSelected, setLanguageSelected] = useState("");
   const [code, setCode] = useState("");
   const [title, setTitle] = useState("");
-  const [result, setResult] = useState(""); // New state for result
+  const [result, setResult] = useState("");
 
   const languageEntries = Object.entries(Language_version);
 
@@ -52,7 +52,7 @@ export default function FetchCodeEditor(props: PropsType) {
     setLanguageSelected(snippet.language);
     setCode(snippet.code);
     setTitle(snippet.title);
-    setResult(snippet.result); // Set result when editing a snippet
+    setResult(snippet.result);
   };
 
   const handleDeleteClick = (index: number) => {
@@ -66,104 +66,101 @@ export default function FetchCodeEditor(props: PropsType) {
         language: languageSelected,
         code: code,
         title: title,
-        result: result, // Include result in the new snippet
+        result: result,
       };
 
       setProgramingLanguage([...programingLanguage, newSnippet]);
       setCode("");
       setTitle("");
-      setResult(""); // Clear result after adding
+      setResult("");
       setLanguageSelected("");
     }
   };
 
   useEffect(() => {
-    console.log("1111", individualSubjectDataList);
-
     setProgramingLanguage(individualSubjectDataList?.programingLanguage);
   }, [individualSubjectDataList]);
+
   return (
     <>
-      <div>
-        <u className="text-center flex items-center justify-center w-full text-2xl p-2">
-          Enter the Programming Code
-        </u>
-        <div className="flex gap-3">
-          {programingLanguage.map((snippet, i) => (
-            <div
-              className="border-2 border-black p-2 m-1 cursor-pointer flex gap-2 hover:bg-black hover:text-white "
-              key={i}
-            >
-              <div onClick={() => handleCodeClick(i)}>{`${snippet.title}
-              `}</div>
-              {/* <div onClick={() => handleDeleteClick(i)}>✖</div> */}
-            </div>
-          ))}
-        </div>
-        <div className="flex md:flex-row flex-col">
-          <div className="md:flex flex-col md:w-1/2 w-full bg-[#1E1E1E] mx-3 ">
-            <input
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder="Enter title"
-              className="p-2 m-2 bg-white border"
-              disabled
-            />
-            <Select
-              value={languageSelected}
-              onValueChange={setLanguageSelected}
-            >
-              <SelectTrigger className="flex w-1/2 m-3 bg-gray-400">
-                <SelectValue placeholder="Select a Language" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Select Language</SelectLabel>
-                  {languageEntries.map(([language, version], i) => (
-                    <SelectItem
-                      value={language}
-                      key={i}
-                      className="flex flex-row w-full"
-                    >
-                      <div className="flex items-center">
-                        {language}
-                        &nbsp;(<sub>{version}</sub>)
-                      </div>
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-
-            <Editor
-              // height="40vh"
-              className="min-h-[20vh]"
-              width={"100%"}
-              language={languageSelected}
-              theme="vs-dark"
-              value={code}
-              onMount={mountFn}
-              onChange={(value) => {
-                setCode(value || "");
-              }}
-            />
+      {programingLanguage.length > 0 && (
+        <div>
+          <u className="text-center flex items-center justify-center w-full text-2xl p-2">
+            Enter the Programming Code
+          </u>
+          <div className="flex gap-3">
+            {programingLanguage
+              .filter((snippet) => snippet.title && snippet.code)
+              .map((snippet, i) => (
+                <div
+                  className="border-2 border-black p-2 m-1 cursor-pointer flex gap-2 hover:bg-black hover:text-white"
+                  key={i}
+                >
+                  <div
+                    onClick={() => handleCodeClick(i)}
+                  >{`${snippet.title}`}</div>
+                  <div onClick={() => handleDeleteClick(i)}>✖</div>
+                </div>
+              ))}
           </div>
-          <Output editorRef={editorRef} languageSelected={languageSelected} />
-        </div>
-        <Input
-          type="text"
-          value={result}
-          onChange={(e) => setResult(e.target.value)}
-          placeholder="Enter result"
-          className="m-3 p-2"
-          disabled
-        />
-      </div>
+          <div className="flex md:flex-row flex-col">
+            <div className="md:flex flex-col md:w-1/2 w-full bg-[#1E1E1E] mx-3 ">
+              <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Enter title"
+                className="p-2 m-2 bg-white border"
+              />
+              <Select
+                value={languageSelected}
+                onValueChange={setLanguageSelected}
+              >
+                <SelectTrigger className="flex w-1/2 m-3 bg-gray-400">
+                  <SelectValue placeholder="Select a Language" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select Language</SelectLabel>
+                    {languageEntries.map(([language, version], i) => (
+                      <SelectItem
+                        value={language}
+                        key={i}
+                        className="flex flex-row w-full"
+                      >
+                        <div className="flex items-center">
+                          {language} <sub>({version})</sub>
+                        </div>
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
 
-      {/* <Button type="button" onClick={handleAddAllCode}>
-        Add Code
-      </Button> */}
+              <Editor
+                className="min-h-[20vh]"
+                width={"100%"}
+                language={languageSelected}
+                theme="vs-dark"
+                value={code}
+                onMount={mountFn}
+                onChange={(value) => setCode(value || "")}
+              />
+            </div>
+            <Output editorRef={editorRef} languageSelected={languageSelected} />
+          </div>
+          <Input
+            type="text"
+            value={result}
+            onChange={(e) => setResult(e.target.value)}
+            placeholder="Enter result"
+            className="m-3 p-2"
+          />
+          {/* <Button type="button" onClick={handleAddAllCode}>
+          Add Code
+        </Button> */}
+        </div>
+      )}
     </>
   );
 }
